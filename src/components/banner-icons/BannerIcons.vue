9d41cbfeb28f85ee944af8575789983ef095e2f7
x-thence <v_xxxliao@tencent.com>
2021-07-20 10:07:51 +0800

    feat: 将首页banner区域的icon和附近商家替换成从接口获取

--- src/components/banner-icons/BannerIcons.vue
+++ src/components/banner-icons/BannerIcons.vue
@@ -14,73 +14,32 @@
 </template>
 
 <script>
-import { Swipe, SwipeItem } from 'vant'
+import { reactive, computed } from 'vue'
+import Request from '../../api/request'
+// 获取iconList列表
+const getIcons = async (iconsInfo) => {
+  const res = await Request.get('/v1/get_banner_list')
+  if (res?.data?.code === 0) {
+    iconsInfo.iconList = res.data.iconList
+  }
+}
+
 export default {
   name: 'BannerIcons',
-  data () {
-    return {
-      iconList: [{
-        text: '超市',
-        imgUrl: 'https://img30.360buyimg.com/mobilecms/jfs/t1/152758/37/12989/8884/5fefa67cE1244a6ad/6495bed0f22bbde8.png'
-      }, {
-        text: '生鲜果蔬',
-        imgUrl: 'https://img30.360buyimg.com/mobilecms/jfs/t1/167276/11/20272/10444/60818dd4Efd444996/b431a72066c3c32a.png'
-      }, {
-        text: '乳制品',
-        imgUrl: 'https://img30.360buyimg.com/mobilecms/jfs/t1/170117/14/17884/8232/6073c058E860b7549/4608bb5de6206c3a.png'
-      }, {
-        text: '个洗家清',
-        imgUrl: 'https://img30.360buyimg.com/mobilecms/jfs/t1/175687/6/5265/8891/607cebf4E00fa2549/bffe07ca1dff3a62.png'
-      }, {
-        text: '粮油副食',
-        imgUrl: 'https://img30.360buyimg.com/mobilecms/jfs/t1/188383/30/7677/9386/60c308b2E2b4b259f/45d803a575430c43.png'
-      }, {
-        text: '送药上门',
-        imgUrl: 'https://img30.360buyimg.com/mobilecms/jfs/t1/188174/11/5016/11161/60ade730E7623640d/f7edff109d18dd3c.png'
-      }, {
-        text: '休闲食品',
-        imgUrl: 'https://img30.360buyimg.com/mobilecms/jfs/t1/191096/7/59/9428/608623ebE86bd6017/a5126fa69bf0ce19.png'
-      }, {
-        text: '酒水饮料',
-        imgUrl: 'https://img30.360buyimg.com/mobilecms/jfs/t1/173730/23/1250/10393/60642179E8ac04847/266e4ae9b63903dc.png'
-      }, {
-        text: '美妆护肤',
-        imgUrl: 'https://img30.360buyimg.com/mobilecms/jfs/t1/184985/2/8411/9355/60c185fdE41f7b5d3/2225851aa2899ede.png'
-      }, {
-        text: '手机家电',
-        imgUrl: 'https://img30.360buyimg.com/mobilecms/jfs/t1/159406/27/1106/9009/5fefa7e5E63f2e1c7/060bb24dfd00503d.png'
-      }, {
-        text: '母婴',
-        imgUrl: 'https://img30.360buyimg.com/mobilecms/jfs/t1/157553/6/17409/9501/606d23fdE45fb5f3d/3f9dee8a2b763a57.png'
-      }, {
-        text: '家居时尚',
-        imgUrl: 'https://img30.360buyimg.com/mobilecms/jfs/t1/166925/40/426/8978/5fefa762E72873d6a/29717e548c12ca36.png'
-      }, {
-        text: '烘焙蛋糕',
-        imgUrl: 'https://img30.360buyimg.com/mobilecms/jfs/t1/158704/3/1167/10639/5fefa77cE72b13dff/9dde706f7245ca15.png'
-      }, {
-        text: '鲜花礼品',
-        imgUrl: 'https://img30.360buyimg.com/mobilecms/jfs/t1/156893/15/3772/10566/5fefa71fEef5f1440/d0345e42d7b32a41.png'
-      }, {
-        text: '平台公示',
-        imgUrl: 'https://img30.360buyimg.com/mobilecms/jfs/t1/169836/11/438/9541/5fefa9c2E3a698661/6be42a0c5f195166.png'
-      }]
-    }
-  },
-  computed: {
-    swipeItems () {
-      const count = Math.ceil(this.iconList.length / 10)
+  setup () {
+    const iconsInfo = reactive({ iconList: [] })
+    getIcons(iconsInfo)
+    // 分割iconList
+    const swipeItems = computed(() => {
+      const count = Math.ceil(iconsInfo.iconList.length / 10)
       // 把iconList切割成10个一组的数组
       const arr = []
       for (let i = 0; i < count; i++) {
-        arr[i] = this.iconList.slice(i * 10, (i + 1) * 10)
+        arr[i] = iconsInfo.iconList.slice(i * 10, (i + 1) * 10)
       }
       return arr
-    }
-  },
-  components: {
-    [Swipe.name]: Swipe,
-    [SwipeItem.name]: SwipeItem
+    })
+    return { swipeItems }
   }
 }
 </script>
--- src/components/nearBy/NearBy.vue
+++ src/components/nearBy/NearBy.vue
@@ -7,53 +7,23 @@
 </template>
 
 <script>
+import { reactive, toRefs } from 'vue'
+import Request from '../../api/request'
 import Merchant from '../merchant/Merchant'
 export default {
   name: 'NearBy',
   components: { Merchant },
   setup () {
-    const nearBys = [
-      {
-        id: '01',
-        imgUrl: 'https://img30.360buyimg.com/vendersettle/s120x120_jfs/t1/104763/10/17095/195874/5e83ecc2Ec39ed4d9/aaf4060a10ac1cc5.png',
-        title: '沃尔玛-福星店',
-        rateValue: 4.5,
-        priceText: '起送¥0  运费¥10',
-        desc: '不含运费满119元，返89-12券/限量'
-      },
-      {
-        id: '02',
-        imgUrl: 'https://img30.360buyimg.com/vendersettle/s120x120_jfs/t1/164976/33/8309/6745/6062d823Ec5b853b7/8316462bae32fc47.png',
-        title: '永旺-新洲店',
-        rateValue: 4,
-        priceText: '起送¥0  运费¥10',
-        desc: '不含运费满119元，返89-12券/限量'
-      },
-      {
-        id: '03',
-        imgUrl: 'https://img30.360buyimg.com/vendersettle/s120x120_jfs/t17815/10/1165301958/17857/34565043/5abdf45bNd33a42d1.jpg',
-        title: '山姆会员商店(华强)',
-        rateValue: 4.5,
-        priceText: '起送¥0  运费¥10',
-        desc: '不含运费满119元，返89-12券/限量'
-      },
-      {
-        id: '04',
-        imgUrl: 'https://img30.360buyimg.com/vendersettle/s120x120_jfs/t1/90010/33/7051/78456/5df88610E16c5f1d8/f7474a271166c9bc.jpg',
-        title: '华润万家-益田店',
-        rateValue: 5,
-        priceText: '起送¥0  运费¥10',
-        desc: '不含运费满119元，返89-12券/限量'
-      },
-      {
-        id: '05',
-        imgUrl: 'https://img30.360buyimg.com/vendersettle/s120x120_jfs/t1/88008/39/15296/65542/5e6fbf28E9e091c31/2950b23f68fa49f1.jpg',
-        title: 'Ole精品超市（深业上城店）',
-        rateValue: 3.5,
-        priceText: '起送¥0  运费¥10',
-        desc: '不含运费满119元，返89-12券/限量'
+    // 获取附近商家列表
+    const nearByInfo = reactive({ nearBys: [] })
+    const getNearBys = async () => {
+      const res = await Request.get('/v1/get_nearby_list')
+      if (res.data.code === 0) {
+        nearByInfo.nearBys = res.data.nearby_list
       }
-    ]
+    }
+    getNearBys()
+    const { nearBys } = toRefs(nearByInfo)
     return { nearBys }
   }
 }
