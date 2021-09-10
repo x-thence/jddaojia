38628cbc100d2a8221575848e023ec8b89a1ec0a
x-thence <2668812066@qq.com>
2021-09-10 23:14:26 +0800

    feat: 修改个人中心页面

--- src/views/personal/Personal.vue
+++ src/views/personal/Personal.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="">
-    个人中心
+  <div class="personal">
+    <div class="personal__header">个人中心</div>
+    <div class="personal__avtar">
+      <img src="@/assets/images/user.png" alt="头像">
+    </div>
     <docker :currentIndex="3" />
   </div>
 </template>
@@ -14,5 +17,25 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.personal{
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: #eee;
+  &__header {
+    line-height: .4rem;
+    text-align: center;
+    background: #fff;
+  }
+  &__avtar {
+    width: .8rem;
+    height: .8rem;
+    margin: auto;
+    img {
+      width: 100%;
+    }
+  }
+}
 </style>
